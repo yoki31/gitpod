0080cffd7bd834a4d49a8560d7436633dcc62341
George Tsiolis <tsiolis.g@gmail.com>
2021-08-09 21:27:15 +0000

    Mention username who added project in a team

--- components/gitpod-protocol/src/gitpod-service.ts
+++ components/gitpod-protocol/src/gitpod-service.ts
@@ -267,7 +267,7 @@ export interface ProviderRepository {
     installationId?: number;
     installationUpdatedAt?: string;
 
-    inUse?: boolean;
+    inUse?: { userName: string };
 }
 
 export interface ClientHeaderFields {
